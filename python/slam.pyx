cef0c3290f990baa400465b73dca54eb0cbe8134
Thiago Teixeira Santos <thiago.santos@embrapa.br>
2018-03-07 00:17:35 -0300

    Fixing Cython file slam.pyx

--- python/slam.pyx
+++ python/slam.pyx
@@ -33,7 +33,7 @@ cdef extern from "System.h" namespace "ORB_SLAM2":
 
   cdef cppclass System:
 
-    enum eSensor "eSensor":
+    enum eSensor:
       MONOCULAR = 0
       STEREO = 1
       RGBD = 2
